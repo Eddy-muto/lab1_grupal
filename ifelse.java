03c7a8712c727195df9713b75860798ae829e9d6
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-06-02 19:48:46 -0500

    agregue ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+public class ifelse {
+    public static void main(String[] args){
+ 	int x=10;
+	int y=4;
+	if(x>y){
+		System.out.println("hola mundo");
+	}
+
+	int time=20;
+	if(time<18){
+		System.out.println("good day");
+	}
+	else{
+		System.out.println("Good evening");
+
+	}
+    }
+}
