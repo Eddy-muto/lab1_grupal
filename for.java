aeaceea8f5116661de7c9489778c16ea131d5221
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-06-02 19:56:41 -0500

    agregue for.java

---
+++ for.java
@@ -0,0 +1,11 @@
+public class for{
+	public static void main(String[] args){
+	for(int i = 0; i <= 5; i ++){
+			System.out.println("YES");
+		}
+	String[] autos = {"Volvo", "BMW", "Ford", "Mazda"};
+		for(String i : autos){
+			System.out.println(i);
+		}
+	}
+}
