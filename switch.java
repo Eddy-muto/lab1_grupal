2c411a769a8e802499fbd39713ed4e82a657b075
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-06-02 19:51:09 -0500

    agregue switch.java

---
+++ switch.java
@@ -0,0 +1,34 @@
+public class Switch {
+    public static void main(String[] args){
+   	int dia = 1;
+        switch (elDia) {
+            case 1:
+                System.out.println("Lunes");
+            break;
+        
+            case 2:
+                System.out.println("Martes");
+            break;
+        
+            case 3:
+                System.out.println("Miercoles");
+            break;
+        
+            case 4:
+                System.out.println("Jueves");
+            break;
+        
+            case 5:
+                System.out.println("Viernes");
+            break;
+  
+            case 6:
+                System.out.println("Sabado");
+            break;
+  
+            case 7:
+                System.out.println("Domingo");
+            break;
+        }
+    }
+}
