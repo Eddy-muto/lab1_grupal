4b3580781711702d641a440f8c76e9ebd44b3a06
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-06-02 19:06:11 -0500

    agregue output.java

---
+++ output.java
@@ -0,0 +1,12 @@
+public class output{
+
+	public static void main(String[] args){
+	
+	System.out.print("Hello World! ");
+	System.out.print("I will print on the same line.");
+	
+	System.out.println(3 + 3);
+	System.out.println(2 * 5);
+	}
+
+}
