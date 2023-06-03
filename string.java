ab0df6aef0d18214239d374200131a65391f1b78
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-06-02 19:34:21 -0500

    agregue string.java

---
+++ string.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args){
+ 	 String saludo = "Holaa";
+	  int longitud = saludo.length();
+	 System.out.println(saludo);
+	 System.out.println(longitud);
+	 System.out.println(saludo.toUpperCase());
+	 System.out.println(saludo.toLowerCase());
+	 System.out.println(saludo.indexOf("h"));
+	
+    }
+}
