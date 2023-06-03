9bdeb370d93ac2812eadbeae5bbbfb9e2b4d39c7
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-06-02 19:21:16 -0500

    agregue typeCasting.java

---
+++ typeCasting.java
@@ -0,0 +1,12 @@
+public typeCasting {
+
+	public static void main(String[] args) {
+	int myInt = 9;
+    	double myDouble = myInt; // hacemos una conversion 
+
+    System.out.println(myInt);      // salida 9
+    System.out.println(myDouble);   // salida 9.0
+  }
+}
+
+}
