a7fb5515fefe1e6418553f6178482e8a16f736c0
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-06-02 19:24:56 -0500

    agregue operators.java

---
+++ operators.java
@@ -0,0 +1,8 @@
+public class Operators {
+    public static void main(String[] args){
+ 	 System.out.println("suma "+2+6);
+	 System.out.println("resta "+2-6);
+	 System.out.println("divicion"+6/2);
+	 System.out.println("multiplicacion"+2*6);
+    }
+}
