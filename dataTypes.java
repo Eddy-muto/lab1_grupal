87908b07b1dc447e775a644ee691d22f8abb4d35
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-06-02 19:17:13 -0500

    agregue datTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,11 @@
+public class dataTypes {
+    public static void main(String[] args){
+        
+        int numero = 53;               
+        float flotante = 4.39f;   
+        char caracter = 'G';        
+        boolean booleano = true;       
+        String cadena = "Hello";   
+ 
+    }
+}
