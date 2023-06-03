95aacaae8388f07742eddb8a49185188c6904a9a
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-06-02 19:44:07 -0500

    agregue booleans.java

---
+++ booleans.java
@@ -0,0 +1,10 @@
+public class booleans {
+	 public static void main(String[] args){
+	 int x = 10;
+         int y = 9;
+         System.out.println(x < y); //retornara FALSE
+	 int miEdad = 23;
+         int mayorEdad = 18;
+        System.out.println(miEdad >= edadParaVotar); //Retornara TRUE
+	 }
+}
