503b140c121f748dfc289aba1e2b6747c05ee8d8
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-06-02 19:13:54 -0500

    agregue variables.java

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class Variables{
+    public static void main(String[] args){
+    	String carName=volvo;
+    	System.out.println(cardName);
+	int x=5;
+	int y=6;
+	System.out.println(x+y);
+	int x = 5, y = 6, z = 50;
+	 System.out.println(x+y+z);
+
+
+    }
+}
+
